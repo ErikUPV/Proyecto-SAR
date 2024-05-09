e04fddc21da3d702a470bb4c30b4b39cd20830b2
MaikTheTurboPotato <miquelgc2003@gmail.com>
2024-05-09 22:27:55 +0200

    Condición para devolver postings

--- SAR_lib.py
+++ SAR_lib.py
@@ -704,8 +704,7 @@ class SAR_Indexer:
         ########################################
         ## COMPLETAR PARA TODAS LAS VERSIONES ##
         ########################################
-        pos = len(self.tokenize(term)) > 1
-        if pos:
+        if len(self.tokenize(term)) > 1:
             # Si hay más de una palabra en el termiod 
             return self.get_positionals(self.tokenize(term),field)
         elif '*' in term or '?' in term:
@@ -715,7 +714,7 @@ class SAR_Indexer:
             # Si está activado el stemming
             return self.get_stemming(term,field)
         else: 
-            if pos:
+            if self.positional:
                 # Si no hay ninguna opción activada para el término pero se ha contruido con posicionales
                 # Cada token tiene una lista con forma [ (artId,[ocrurrencias]), (artId,[ocrurrencias]),...] 
                 if term not in self.index[field]:
