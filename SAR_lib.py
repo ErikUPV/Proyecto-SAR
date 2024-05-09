efc16fb96d22cda23293b3be02eb880d5821ea9d
ErikUPV <119046735+ErikUPV@users.noreply.github.com>
2024-05-09 18:02:54 +0200

    ahora devuelve la lista ordenada

--- SAR_lib.py
+++ SAR_lib.py
@@ -406,7 +406,7 @@ class SAR_Indexer:
                
             for token in self.index[field].keys():
                 self.ptindex[field].extend(sorted((f'{token[j:]}${token[:j]}', token) for j in range(len(token)+1)))
-            self.ptindex[field] = list(set(self.ptindex[field]))
+            self.ptindex[field] = sorted(list(set(self.ptindex[field])))
 
     def show_stats(self):
         """
