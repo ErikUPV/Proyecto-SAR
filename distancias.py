a7bfe5de3203a82396af6d91538f30c40f3fdd7f
ErikUPV <119046735+ErikUPV@users.noreply.github.com>
2024-10-11 13:10:42 +0200

    Metodo levehenstein_reduccion y test_distancias

--- distancias.py
+++ distancias.py
@@ -49,7 +49,25 @@ def levenshtein_edicion(x, y, threshold=None): #ALEX
 
 def levenshtein_reduccion(x, y, threshold=None): #ERIK
     # completar versión con reducción coste espacial
-    return 0 # COMPLETAR Y REEMPLAZAR ESTA PARTE
+    lenX, lenY = len(x), len(y)
+    vcurrent = np.zeros(lenX + 1, dtype=np.int)
+    vprev = np.zeros(lenX + 1, dtype=np.int)
+
+    #B = np.zeros((lenX + 1, lenY + 1), dtype=np.int)
+    for i in range (1, lenX + 1):
+        vcurrent[i] = vcurrent[i - 1] + 1
+        #B[i][0] = (i - 1, 0)
+    for j in range(1, lenY + 1):
+        vcurrent[0] = vprev[0] + 1
+        #B[0][j] = (0, j - 1)
+        for i in range(1, lenX + 1):
+            vcurrent[i][j] = min(
+                vcurrent[i - 1] + 1,
+                vprev[i] + 1,
+                vprev[i - 1] + (x[i - 1] != y[j - 1]),
+            )
+        vcurrent, vprev = vprev, vcurrent
+    return vcurrent[lenX]
 
 def levenshtein(x, y, threshold): #HECTOR
     # completar versión reducción coste espacial y parada por threshold
